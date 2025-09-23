59f99de85072f05b662e7adf3072bbf73da41795
Md Nur Ahmad <234045679+arionysbd@users.noreply.github.com>
2025-09-24 01:39:58 +0600

    init

---
+++ Mahdi.Website/src/lib/content/portfolio.ts
@@ -0,0 +1,42 @@
+/*
+ * =========================
+ * AUTHOR INFO
+ * =========================
+ */
+
+import { StringKeyValueType } from '../types';
+
+export const socialLinks: StringKeyValueType = {
+  facebook: 'https://www.facebook.com/mahdibinferdaus',
+  instagram: 'https://www.instagram.com/mahdibinferdaus/',
+  twitter: 'https://x.com/mahdi_tech_',
+  github: 'https://github.com/mahdibinferdaus',
+  linkedin: 'https://www.linkedin.com/in/mahdibinferdaus/',
+  youtube: 'https://www.youtube.com/@MahdiTheTechGuy',
+  linktree: 'https://linktr.ee/mahdibin',
+};
+
+export const author = {
+  name: 'Mahdi Bin Ferdaus',
+  email: 'ibnemahdi5@gmail.com',
+};
+
+export const seoData = {
+  title: 'Mahdi Bin Ferdaus | Hardware Engineer & Developer',
+  description:
+    'Mahdi Bin Ferdaus is a hardware engineer and developer who specializes in building innovative solutions at the intersection of hardware and software.',
+  author: author.name,
+  image:
+    'https://user-images.githubusercontent.com/68834718/217467445-0b0d2d63-f8ad-4702-8474-814eb2cbb3be.png',
+  url: 'https://nadimshahriar.vercel.app/',
+  keywords: [
+    'Nadim',
+    'Mahdi Bin Ferdaus',
+    'Hardware Engineer',
+    'Developer',
+    'Bangladesh',
+    'Portfolio',
+    'Nadim Portfolio',
+    'Mahdi Bin Ferdaus Portfolio',
+  ],
+};
